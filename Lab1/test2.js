6c1f878d2a1b7d4d95ca9532ef6abeac7793c585
Barniclassic <76923911+Barniclassic@users.noreply.github.com>
2021-10-26 17:56:35 -0500

    Update test2.js

--- Lab1/test2.js
+++ Lab1/test2.js
@@ -1,6 +1,6 @@
 console.log("This is test 2");
 console.log("This is test 2 - for master and user line 2");
 
-function foo() {
-    console.log("This is test 2 from origin main");
+function gefgef() {
+    console.log("This is updated from origin");
 }
